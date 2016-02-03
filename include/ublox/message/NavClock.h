166e699f0683a13d5214ccb410ed9bcc96bffb1b
Alex Robenko <arobenko@gmail.com>
2016-02-03 18:07:34 +1000

    Documentation for NAV-CLOCK messages.

--- include/ublox/message/NavClock.h
+++ include/ublox/message/NavClock.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2015 (C). Alex Robenko. All rights reserved.
+// Copyright 2015 - 2016 (C). Alex Robenko. All rights reserved.
 //
 
 // This file is free software: you can redistribute it and/or modify
@@ -15,12 +15,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+/// @file
+/// @brief Contains definition of NAV-CLOCK message and its fields.
 
 #pragma once
 
-#include "comms/Message.h"
 #include "ublox/Message.h"
-
 #include "ublox/field/nav.h"
 
 namespace ublox
@@ -29,34 +29,56 @@ namespace ublox
 namespace message
 {
 
-using NavClockField_iTOW = field::nav::iTOW;
-using NavClockField_clkB = field::common::I4T<field::common::Scaling_ns2s>;
-using NavClockField_clkD = field::common::I4T<field::common::Scaling_ns2s>;
-using NavClockField_tAcc = field::nav::tAcc;
-using NavClockField_fAcc = field::common::U4;
+/// @brief Accumulates details of all the NAV-CLOCK message fields.
+/// @see NavClock
+struct NavClockFields
+{
+    /// @brief Definition of "iTOW" field.
+    using iTOW = field::nav::iTOW;
 
-using NavClockFields = std::tuple<
-    NavClockField_iTOW,
-    NavClockField_clkB,
-    NavClockField_clkD,
-    NavClockField_tAcc,
-    NavClockField_fAcc
->;
+    /// @brief Definition of "clkB" field.
+    using clkB = field::common::I4T<field::common::Scaling_ns2s>;
 
+    /// @brief Definition of "clkD" field.
+    using clkD = field::common::I4T<field::common::Scaling_ns2s>;
 
+    /// @brief Definition of "tAcc" field.
+    using tAcc = field::nav::tAcc;
+
+    /// @brief Definition of "fAcc" field.
+    using fAcc = field::common::U4;
+
+    /// @brief All the fields bundled in std::tuple.
+    using All = std::tuple<
+        iTOW,
+        clkB,
+        clkD,
+        tAcc,
+        fAcc
+    >;
+};
+
+/// @brief Definition of NAV-CLOCK message
+/// @details Inherits from
+///     <a href="https://dl.dropboxusercontent.com/u/46999418/comms_champion/comms/html/classcomms_1_1MessageBase.html">comms::MessageBase</a>
+///     while providing @b TMsgBase as common interface class as well as
+///     @b comms::option::StaticNumIdImpl, @b comms::option::FieldsImpl, and
+///     @b comms::option::DispatchImpl as options. @n
+///     See @ref NavClockFields and for definition of the fields this message contains.
+/// @tparam TMsgBase Common interface class for all the messages.
 template <typename TMsgBase = Message>
 class NavClock : public
     comms::MessageBase<
         TMsgBase,
         comms::option::StaticNumIdImpl<MsgId_NAV_CLOCK>,
-        comms::option::FieldsImpl<NavClockFields>,
+        comms::option::FieldsImpl<NavClockFields::All>,
         comms::option::DispatchImpl<NavClock<TMsgBase> >
     >
 {
     typedef comms::MessageBase<
         TMsgBase,
         comms::option::StaticNumIdImpl<MsgId_NAV_CLOCK>,
-        comms::option::FieldsImpl<NavClockFields>,
+        comms::option::FieldsImpl<NavClockFields::All>,
         comms::option::DispatchImpl<NavClock<TMsgBase> >
     > Base;
 public:
@@ -64,11 +86,11 @@ public:
     /// @brief Index to access the fields
     enum FieldIdx
     {
-        FieldIdx_iTOW,
-        FieldIdx_clkB,
-        FieldIdx_clkD,
-        FieldIdx_tAcc,
-        FieldIdx_fAcc,
+        FieldIdx_iTOW, ///< @b iTOW field, see @ref NavClockFields::iTOW
+        FieldIdx_clkB, ///< @b clkB field, see @ref NavClockFields::clkB
+        FieldIdx_clkD, ///< @b clkD field, see @ref NavClockFields::clkD
+        FieldIdx_tAcc, ///< @b tAcc field, see @ref NavClockFields::tAcc
+        FieldIdx_fAcc, ///< @b fAcc field, see @ref NavClockFields::fAcc
         FieldIdx_numOfValues ///< number of available fields
     };
 
