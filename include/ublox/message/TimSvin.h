8f7cf6a758764b5ca85dde2a65292520ac67a92a
Alex Robenko <arobenko@gmail.com>
2016-02-08 18:10:00 +1000

    Documentation for TIM-SVIN messages.

--- include/ublox/message/TimSvin.h
+++ include/ublox/message/TimSvin.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2015 (C). Alex Robenko. All rights reserved.
+// Copyright 2015 - 2016 (C). Alex Robenko. All rights reserved.
 //
 
 // This file is free software: you can redistribute it and/or modify
@@ -15,10 +15,11 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+/// @file
+/// @brief Contains definition of TIM-SVIN message and its fields.
 
 #pragma once
 
-#include "comms/Message.h"
 #include "ublox/Message.h"
 #include "ublox/field/common.h"
 
@@ -28,41 +29,72 @@ namespace ublox
 namespace message
 {
 
-using TimSvinField_dur = field::common::U4;
-using TimSvinField_meanX = field::common::I4T<field::common::Scaling_cm2m>;
-using TimSvinField_meanY = TimSvinField_meanX;
-using TimSvinField_meanZ = TimSvinField_meanX;
-using TimSvinField_meanV = field::common::U4T<comms::option::ScalingRatio<1, 1000000L> >;
-using TimSvinField_obs = field::common::U4;
-using TimSvinField_valid = field::common::U1;
-using TimSvinField_active = field::common::U1;
-using TimSvinField_reserved = field::common::res2;
+/// @brief Accumulates details of all the TIM-SVIN message fields.
+/// @see TimSvin
+struct TimSvinFields
+{
+    /// @brief Definition of "dur" field.
+    using dur = field::common::U4;
 
-using TimSvinFields = std::tuple<
-    TimSvinField_dur,
-    TimSvinField_meanX,
-    TimSvinField_meanY,
-    TimSvinField_meanZ,
-    TimSvinField_meanV,
-    TimSvinField_obs,
-    TimSvinField_valid,
-    TimSvinField_active,
-    TimSvinField_reserved
->;
+    /// @brief Definition of "meanX" field.
+    using meanX = field::common::I4T<field::common::Scaling_cm2m>;
 
+    /// @brief Definition of "meanY" field.
+    using meanY = meanX;
+
+    /// @brief Definition of "meanZ" field.
+    using meanZ = meanX;
+
+    /// @brief Definition of "meanV" field.
+    using meanV = field::common::U4T<comms::option::ScalingRatio<1, 1000000L> >;
+
+    /// @brief Definition of "obs" field.
+    using obs = field::common::U4;
+
+    /// @brief Definition of "valid" field.
+    using valid = field::common::U1;
+
+    /// @brief Definition of "active" field.
+    using active = field::common::U1;
+
+    /// @brief Definition of "reserved" field.
+    using reserved = field::common::res2;
+
+    /// @brief All the fields bundled in std::tuple.
+    using All = std::tuple<
+        dur,
+        meanX,
+        meanY,
+        meanZ,
+        meanV,
+        obs,
+        valid,
+        active,
+        reserved
+    >;
+};
+
+/// @brief Definition of TIM-SVIN message
+/// @details Inherits from
+///     <a href="https://dl.dropboxusercontent.com/u/46999418/comms_champion/comms/html/classcomms_1_1MessageBase.html">comms::MessageBase</a>
+///     while providing @b TMsgBase as common interface class as well as
+///     @b comms::option::StaticNumIdImpl, @b comms::option::FieldsImpl, and
+///     @b comms::option::DispatchImpl as options. @n
+///     See @ref TimSvinFields and for definition of the fields this message contains.
+/// @tparam TMsgBase Common interface class for all the messages.
 template <typename TMsgBase = Message>
 class TimSvin : public
     comms::MessageBase<
         TMsgBase,
         comms::option::StaticNumIdImpl<MsgId_TIM_SVIN>,
-        comms::option::FieldsImpl<TimSvinFields>,
+        comms::option::FieldsImpl<TimSvinFields::All>,
         comms::option::DispatchImpl<TimSvin<TMsgBase> >
     >
 {
     typedef comms::MessageBase<
         TMsgBase,
         comms::option::StaticNumIdImpl<MsgId_TIM_SVIN>,
-        comms::option::FieldsImpl<TimSvinFields>,
+        comms::option::FieldsImpl<TimSvinFields::All>,
         comms::option::DispatchImpl<TimSvin<TMsgBase> >
     > Base;
 public:
@@ -70,15 +102,15 @@ public:
     /// @brief Index to access the fields
     enum FieldIdx
     {
-        FieldIdx_dur,
-        FieldIdx_meanX,
-        FieldIdx_meanY,
-        FieldIdx_meanZ,
-        FieldIdx_meanV,
-        FieldIdx_obs,
-        FieldIdx_valid,
-        FieldIdx_active,
-        FieldIdx_reserved,
+        FieldIdx_dur, ///< @b dur field, see @ref TimSvinFields::dur
+        FieldIdx_meanX, ///< @b meanX field, see @ref TimSvinFields::meanX
+        FieldIdx_meanY, ///< @b meanY field, see @ref TimSvinFields::meanY
+        FieldIdx_meanZ, ///< @b meanZ field, see @ref TimSvinFields::meanZ
+        FieldIdx_meanV, ///< @b meanV field, see @ref TimSvinFields::meanV
+        FieldIdx_obs, ///< @b obs field, see @ref TimSvinFields::obs
+        FieldIdx_valid, ///< @b valid field, see @ref TimSvinFields::valid
+        FieldIdx_active, ///< @b active field, see @ref TimSvinFields::active
+        FieldIdx_reserved, ///< @b reserved field, see @ref TimSvinFields::reserved
         FieldIdx_numOfValues ///< number of available fields
     };
 
