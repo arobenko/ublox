6742e65139ee15c5c151f69062217179d74dd086
Alex Robenko <arobenko@gmail.com>
2017-01-19 09:14:03 +1000

    Updating access to the fields of INF messages.

--- include/ublox/message/InfStringMsgBase.h
+++ include/ublox/message/InfStringMsgBase.h
@@ -46,7 +46,8 @@ struct InfStringMsgBaseFields
 /// @details Inherits from @b comms::MessageBase
 ///     while providing @b TMsgBase as common interface class as well as
 ///     various implementation options. @n
-///     See @ref InfStringMsgBaseFields and for definition of the fields this message contains.
+///     See @ref InfStringMsgBaseFields and for definition of the fields this message contains
+///         and COMMS_MSG_FIELDS_ACCESS() for fields access details.
 /// @tparam TId ID of actual INF-* message
 /// @tparam TMsgBase Common interface class for all the messages.
 template <MsgId TId, typename TMsgBase = Message>
@@ -66,35 +67,14 @@ class InfStringMsgBase : public
     > Base;
 public:
 
-#ifdef FOR_DOXYGEN_DOC_ONLY
-    /// @brief Index to access the fields
-    enum FieldIdx
-    {
-        FieldIdx_str, ///< @b str field, see @ref InfStringMsgBaseFields::str
-        FieldIdx_numOfValues ///< number of available fields
-    };
-
-    /// @brief Access to fields bundled as a struct
-    struct FieldsAsStruct
-    {
-        InfStringMsgBaseFields::str& str; ///< @b str field, see @ref InfStringMsgBaseFields::str
-    };
-
-    /// @brief Access to @b const fields bundled as a struct
-    struct ConstFieldsAsStruct
-    {
-        const InfStringMsgBaseFields::str& str; ///< @b str field, see @ref InfStringMsgBaseFields::str
-    };
-
-    /// @brief Get access to fields bundled into a struct
-    FieldsAsStruct fieldsAsStruct();
-
-    /// @brief Get access to @b const fields bundled into a struct
-    ConstFieldsAsStruct fieldsAsStruct() const;
-
-#else
+    /// @brief Allow access to internal fields.
+    /// @details See definition of @b COMMS_MSG_FIELDS_ACCESS macro
+    ///     related to @b comms::MessageBase class from COMMS library
+    ///     for details.
+    ///
+    ///     The field names are:
+    ///     @li @b str for InfStringMsgBaseFields::str field
     COMMS_MSG_FIELDS_ACCESS(Base, str);
-#endif // #ifdef FOR_DOXYGEN_DOC_ONLY
 
 
     /// @brief Default constructor
