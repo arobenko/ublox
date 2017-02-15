2e960bbf409653cf62d7d68bfa070fa41d8800bc
Alex Robenko <arobenko@gmail.com>
2017-02-15 18:32:37 +1000

    Implemented CFG-GEOFENCE message.

--- cc_plugin/AllMessages.h
+++ cc_plugin/AllMessages.h
@@ -140,6 +140,7 @@
 #include "cc_plugin/message/CfgLogfilterPoll.h"
 #include "cc_plugin/message/CfgEsrc.h"
 #include "cc_plugin/message/CfgDosc.h"
+#include "cc_plugin/message/CfgGeofence.h"
 #include "cc_plugin/message/CfgFixseed.h"
 #include "cc_plugin/message/CfgDynseed.h"
 #include "cc_plugin/message/MonIo.h"
@@ -322,6 +323,7 @@ typedef std::tuple<
     cc_plugin::message::CfgLogfilterPoll,
     cc_plugin::message::CfgEsrc,
     cc_plugin::message::CfgDosc,
+    cc_plugin::message::Geofence,
     cc_plugin::message::CfgFixseed,
     cc_plugin::message::CfgDynseed,
     cc_plugin::message::MonIo,
