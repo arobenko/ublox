c996c78633f7ef64056f4d09d1a9bd82fc5f6dab
Alex Robenko <arobenko@gmail.com>
2017-08-22 09:17:06 +1000

    Restoring all messages list.

--- cc_plugin/AllMessages.h
+++ cc_plugin/AllMessages.h
@@ -105,7 +105,7 @@
 #include "cc_plugin/message/RxmRlmLong.h"
 #include "cc_plugin/message/RxmImes.h"
 #include "cc_plugin/message/RxmImesPoll.h"
-/*
+
 #include "cc_plugin/message/InfError.h"
 #include "cc_plugin/message/InfWarning.h"
 #include "cc_plugin/message/InfNotice.h"
@@ -330,7 +330,7 @@
 
 #include "cc_plugin/message/HnrPvt.h"
 #include "cc_plugin/message/HnrPvtPoll.h"
-*/
+
 namespace ublox
 {
 
@@ -420,7 +420,7 @@ using AllMessages = std::tuple<
     cc_plugin::message::RxmRlmShort,
     cc_plugin::message::RxmRlmLong,
     cc_plugin::message::RxmImes,
-    cc_plugin::message::RxmImesPoll/*,
+    cc_plugin::message::RxmImesPoll,
     cc_plugin::message::InfError,
     cc_plugin::message::InfWarning,
     cc_plugin::message::InfNotice,
@@ -632,7 +632,7 @@ using AllMessages = std::tuple<
     cc_plugin::message::SecSign,
     cc_plugin::message::SecUniqid,
     cc_plugin::message::HnrPvt,
-    cc_plugin::message::HnrPvtPoll*/
+    cc_plugin::message::HnrPvtPoll
 >;
 
 }  // namespace cc_plugin
