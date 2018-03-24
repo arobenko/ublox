3f9fa608cccfc3e432c3996c764b1449a9d478e7
Alex Robenko <arobenko@gmail.com>
2018-03-24 11:54:33 +1000

    Minor doc fix.

--- include/ublox/version.h
+++ include/ublox/version.h
@@ -16,7 +16,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /// @file
-/// @brief Contains version of th elibrary.
+/// @brief Contains version of the library.
 
 #pragma once
 
